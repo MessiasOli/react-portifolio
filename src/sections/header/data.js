86e5fc568109920ad9a9a7c72e28bd6879e7e871
Messias Oliveira <user@DESKTOP-MESSIAS>
2023-08-24 15:10:45 -0300

    :construction: Constructed Services
    constructed Services

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -6,7 +6,7 @@ import {AiOutlineGithub} from "react-icons/ai"
 const data = [
   {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram />},
   {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter />},
-  {id: 3, link: 'https://linkedin.com', icon: <AiOutlineLinkedin />},
+  {id: 3, link: 'https://www.linkedin.com/in/messias-oliveira-55b264133/', icon: <AiOutlineLinkedin />},
   {id: 4, link: 'https://github.com', icon: <AiOutlineGithub />}
 ]
 
