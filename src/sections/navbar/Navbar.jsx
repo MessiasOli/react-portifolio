0d94ea4176c6bdf286f36b96eea16f575d2798d3
Messias Oliveira <user@DESKTOP-MESSIAS>
2023-08-24 10:03:49 -0300

    :construction: "building navbar"
    "building navbar"

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,25 @@
+import Logo from "../../assets/logo.jpg"
 import "./navbar.css"
+import {IoMdColorPalette} from 'react-icons/io'
+import data from "./data"
 
 function Navbar() {
   return (
-    <nav><h2>NavBar</h2></nav>
+    <nav><div className="container nav__container">
+      <a href="index.html" className="nav__logo">
+        <img src={Logo} alt="Logo" />
+      </a>
+      <ul className="nav__menu">
+      {
+        data.map(item => {
+          return  <li key={item.id}>
+                    <a href={item.link}>{item.title}</a>
+                  </li>
+        })
+      }
+      </ul>
+      <button id="theme__icon"><IoMdColorPalette/></button>
+    </div></nav>
   )
 }
 
