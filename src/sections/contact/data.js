c8201852abba8cccc5405526680cf9b3aa41a42d
Messias Oliveira <user@DESKTOP-MESSIAS>
2023-08-25 12:38:06 -0300

    :construction: Finalized the contacts section
    Finalized the contacts section

---
+++ src/sections/contact/data.js
@@ -0,0 +1,17 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:dev.messias.2019@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/messias.oliveira.92167'},
+    //{id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/qr/465KL4UT6OVGL1'}
+	  {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+5516997391404'}
+]
+
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+
+
+export default data
